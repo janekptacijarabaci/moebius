86c8ce1707c5932173526c828934790117bbd887
wolfbeast <mcwerewolf@gmail.com>
2017-06-26 23:43:05 +0200

    Remove computeNonIncrementalMarkingForValidation(lock) and caller

--- js/src/gc/GCRuntime.h
+++ js/src/gc/GCRuntime.h
@@ -976,7 +976,6 @@ class GCRuntime
     void releaseRelocatedArenasWithoutUnlocking(Arena* arenaList, const AutoLockGC& lock);
     void finishCollection(JS::gcreason::Reason reason);
 
-    void computeNonIncrementalMarkingForValidation(AutoLockForExclusiveAccess& lock);
     void validateIncrementalMarking();
     void finishMarkingValidation();
 
