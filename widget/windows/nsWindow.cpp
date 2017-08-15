e1ed9fed497e5ba6c43bea2ee90a279a88ab2fac
wolfbeast <mcwerewolf@gmail.com>
2017-08-15 17:46:01 +0200

    Use correct window reference when setting parent window in Windows widget.
    SetParent removes the widget from the old parent widget and then sets its new one.
    We were incorrectly using the new parent widget when we needed the old one.

--- widget/windows/nsWindow.cpp
+++ widget/windows/nsWindow.cpp
@@ -1225,13 +1225,14 @@ void nsWindow::SubclassWindow(BOOL bState)
 void
 nsWindow::SetParent(nsIWidget *aNewParent)
 {
-  mParent = aNewParent;
-
   nsCOMPtr<nsIWidget> kungFuDeathGrip(this);
   nsIWidget* parent = GetParent();
   if (parent) {
     parent->RemoveChild(this);
   }
+
+  mParent = aNewParent;
+
   if (aNewParent) {
     ReparentNativeWidget(aNewParent);
     aNewParent->AddChild(this);
