9734b37179599106aa8e1cab7b9a68799c8667c5
janekptacijarabaci <janekptacijarabaci@seznam.cz>
2017-08-22 20:20:39 +0200

    Bug 1168376 (+ bug 1350228 - partially): Show transferred size in request summary instead of decompressed size (+ DOMContentLoaded and load)

--- devtools/client/netmonitor/components/summary-button.js
+++ devtools/client/netmonitor/components/summary-button.js
@@ -8,7 +8,10 @@ const { DOM, PropTypes } = require("devtools/client/shared/vendor/react");
 const { connect } = require("devtools/client/shared/vendor/react-redux");
 const { PluralForm } = require("devtools/shared/plural-form");
 const { L10N } = require("../l10n");
-const { getDisplayedRequestsSummary } = require("../selectors/index");
+const {
+  getDisplayedRequestsSummary,
+  getDisplayedTimingMarker
+} = require("../selectors/index");
 const Actions = require("../actions/index");
 const {
   getSizeWithDecimals,
@@ -20,14 +23,25 @@ const { button, span } = DOM;
 function SummaryButton({
   summary,
   triggerSummary,
+  timingMarkers
 }) {
   let { count, contentSize, transferredSize, millis } = summary;
+  let {
+    DOMContentLoaded,
+    load,
+  } = timingMarkers;
   const text = (count === 0) ? L10N.getStr("networkMenu.empty") :
     PluralForm.get(count, L10N.getStr("networkMenu.summary2"))
     .replace("#1", count)
     .replace("#2", getSizeWithDecimals(contentSize / 1024))
     .replace("#3", getSizeWithDecimals(transferredSize / 1024))
-    .replace("#4", getTimeWithDecimals(millis / 1000));
+    .replace("#4", getTimeWithDecimals(millis / 1000))
+    + ((DOMContentLoaded > -1)
+        ? ", " + "DOMContentLoaded: " + L10N.getFormatStrWithNumbers("networkMenu.timeS", getTimeWithDecimals(DOMContentLoaded / 1000))
+        : "")
+    + ((load > -1)
+        ? ", " + "load: " + L10N.getFormatStrWithNumbers("networkMenu.timeS", getTimeWithDecimals(load / 1000))
+        : "");
 
   return button({
     id: "requests-menu-network-summary-button",
@@ -41,11 +55,17 @@ function SummaryButton({
 
 SummaryButton.propTypes = {
   summary: PropTypes.object.isRequired,
+  timingMarkers: PropTypes.object.isRequired,
 };
 
 module.exports = connect(
   (state) => ({
     summary: getDisplayedRequestsSummary(state),
+    timingMarkers: {
+      DOMContentLoaded:
+        getDisplayedTimingMarker(state, "firstDocumentDOMContentLoadedTimestamp"),
+      load: getDisplayedTimingMarker(state, "firstDocumentLoadTimestamp"),
+    },
   }),
   (dispatch) => ({
     triggerSummary: () => {
