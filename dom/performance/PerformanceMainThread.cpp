62a43c1d5f4aaed2f10611d63b675e023374983a
janekptacijarabaci <janekptacijarabaci@seznam.cz>
2017-09-03 22:27:58 +0200

    DevTools - network - implement the secureConnectionStart property for the PerformanceTiming

--- dom/performance/PerformanceMainThread.cpp
+++ dom/performance/PerformanceMainThread.cpp
@@ -201,7 +201,7 @@ PerformanceMainThread::IsPerformanceTimingAttribute(const nsAString& aName)
   static const char* attributes[] =
     {"navigationStart", "unloadEventStart", "unloadEventEnd", "redirectStart",
      "redirectEnd", "fetchStart", "domainLookupStart", "domainLookupEnd",
-     "connectStart", "connectEnd", "requestStart", "responseStart",
+     "connectStart", "secureConnectionStart", "connectEnd", "requestStart", "responseStart",
      "responseEnd", "domLoading", "domInteractive",
      "domContentLoadedEventStart", "domContentLoadedEventEnd", "domComplete",
      "loadEventStart", "loadEventEnd", nullptr};
@@ -249,6 +249,9 @@ PerformanceMainThread::GetPerformanceTimingFromString(const nsAString& aProperty
   }
   if (aProperty.EqualsLiteral("connectStart")) {
     return Timing()->ConnectStart();
+  }
+  if (aProperty.EqualsLiteral("secureConnectionStart")) {
+    return Timing()->SecureConnectionStart();
   }
   if (aProperty.EqualsLiteral("connectEnd")) {
     return Timing()->ConnectEnd();
